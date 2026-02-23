31ee80ef77dc98ec57409266008f4f3c919ee3c5
shubham kumar <shubhamsinghrajput548@gmail.com>
2026-02-23 12:09:55 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class power {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Math.pow(3.14,3.14));
+        System.out.println(Math.sqrt(3.14));
+        System.out.println(Math.cbrt(3.14));
+        
+    }
+}
